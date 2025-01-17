5fe462cbf293f8f5b73ad6f1c2bcd89812786f10
riya636 <riyaaa.9099@gmail.com>
2025-01-17 21:28:56 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Dynamic testimonial update (optional)
+function updateTestimonial(newText, newAuthor) {
+    document.querySelector('.testimonial-text').innerHTML = newText;
+    document.querySelector('.author-photo').src = newAuthor.photo;
+  }
+  
